1d7445323b2497f36482e149ede1dcece254db3f
Henry <henry@reed.edu>
2019-05-07 16:58:46 -0700

    renamed test cnn

---
+++ final/test-cnn/constants.py
@@ -0,0 +1,6 @@
+model_name        = 'reflected-random-model'
+training_data_dir = 'reflected-random'
+testing_data_dir  = 'reflected-random'
+img_size          = 28
+num_channels      = 1
+num_iterations    = 100
--- final/test-imageclassification/constants.py
+++
@@ -1,7 +0,0 @@
-model_name = 'square-triangle-model'
-training_data_dir = 'square-triangle'
-testing_data_dir = 'square-triangle'
-
-img_size        = 50
-num_channels    = 3
-num_iterations  = 100
