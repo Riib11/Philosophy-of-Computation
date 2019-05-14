2af261ae9d338e56f8062a2401b77560b36735ca
Henry <henry@reed.edu>
2019-05-13 20:42:18 -0700

    added GAN results to final report

---
+++ final/results/gan/graph_results.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+##############################################################################################################################
+# read data
+
+def parse_percent(s):
+    s = s.replace('%','')
+    s = s.replace(',','')
+    return float(s)/100
+
+# read validation accuracies
+discriminator_accuracies = []
+
+experiments = ['solidcolor', 'reflection', 'simpleshape', 'matching']
+
+for name in experiments:
+    fn = '%s_1__log.txt' % name
+    dis_loses = []
+    dis_accus = []
+    gen_loses = []
+    gen_accus = []
+    with open(fn, 'r+') as file:
+        print(fn)
+        lines = [l for l in file]
+        lines = lines[:1000]
+        lines = [ lines[i] for i in range(0, len(lines), 50) ]
+        for line in lines:
+            line = line.split()
+            # dis_loses.append( float(line[3][:-1]) )
+            dis_accus.append( float(line[5][:-1]) )
+            # gen_loses.append( float(line[8][:-1]) )
+            # gen_accus.append( float(line[10][:-1]) )
+    discriminator_accuracies.append((name, dis_accus))
+
+##############################################################################################################################
+# plot data
+
+# setup
+fig, ax = plt.subplots()
+
+# labels
+plt.title('GAN Training Progress')
+plt.ylabel('discriminator accuracy')
+plt.xlabel('iteration')
+
+print(len(discriminator_accuracies[0][1])+1)
+# data
+epochs = np.arange(1, len(discriminator_accuracies[0][1])+1, dtype=int)
+print(epochs)
+
+for name, dis_accus in discriminator_accuracies:
+    ax.plot(epochs, dis_accus, label=name)
+plt.axis([1, epochs[-1],  0, 1.05])
+
+new_labels = epochs
+ax.set_xticklabels(new_labels)
+
+# legend
+legend = ax.legend(loc='lower center')
+
+# show
+plt.show()
